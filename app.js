86c9255073bfd17c6f42fa10dbc9ce5e5acfe564
Naufil Shaikh <snaufil09@gmail.com>
2023-01-03 08:16:30 +0530

    Session 3: Laying the foundation
    1. Added assignment
    2. Create nested header using jsx and react.createElement
    3. Added start/build command in package.json
    4. Added plugin to hide console.log

--- app.js
+++ app.js
@@ -22,31 +22,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading1 = React.createElement(
+const h1 = React.createElement(
     "h1",
     {
-        id: "title1",
+        id: "h1",
+        key: "h1",
     },
-    "Hello from parcel"
+    "Getting"
 );
 
-const heading2 = React.createElement(
+const h2 = React.createElement(
     "h2",
     {
-        id: "title2",
+        id: "h2",
+        key: "h2",
     },
-    "Heading 2"
+    "Started"
 );
 
-const container = React.createElement(
+const h3 = React.createElement(
+    "h3",
+    {
+        id: "h3",
+        key: "h3",
+    },
+    "Namaste React"
+);
+
+const title = React.createElement(
     "div",
     {
-        id: "container",
+        className: "title",
     },
-    [heading1, heading2]
+    [h1, h2, h3]
 );
+
+const jsx = (
+    <div className='title'>
+        <h1 id='jh1'>hello1</h1>
+        <h2 id='jh2'>hello2</h2>
+        <h3 id='jh3'>hello3</h3>
+    </div>
+)
+
+const mergeElements = (
+    <div>
+        {title}
+        {jsx}
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 //passing a react element inside the root
-root.render(container);
+// root.render(title);
+// root.render(jsx);
+root.render(mergeElements);
