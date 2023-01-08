726a9b05318538ad6f313e065d7d695f5ea265aa
Naufil Shaikh <snaufil09@gmail.com>
2023-01-08 16:41:09 +0530

    Chapter 4
    1. Added layout for food delivery app
    2. Re-arrange previous files.

--- app.js
+++ app.js
@@ -1,92 +1,68 @@
-/***
- *
- * Created A Server
- * HMR - Hot Module Replacement
- * File Watcher algorithm - C++
- * BUNDLING
- * MINIFY
- * Cleaning our Code
- * Dev abd Production Build
- * Super Fast build algorithm
- * Image Optimization
- * Caching while development
- * Compression
- * Compatble with older version of browser
- * HTTPS on dev
- * port Number
- * Consistent Hashing Algorithm
- * Zero Config
- * Transitive Dependencies
- */
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//CreateElement
-const h1 = React.createElement(
-    "h1",
-    {
-        id: "h1",
-        key: "h1",
-    },
-    "Getting"
-);
+/** 
+   Header
+    - Logo(Title)
+    - Nav Items
+    - Cart
+   Body
+    - Search bar
+    - RestaurantList
+        - RestaurantCard (Multiple)
+            - Image
+            - Name
+            - Rating 
+            - Cusines
+    Footer
+        - Links
+        - Copyright
+*/
 
-const h2 = React.createElement(
-    "h2",
-    {
-        id: "h2",
-        key: "h2",
-    },
-    "Started"
-);
+const Title = () => {
+    <a href='/'>
+        <img
+            className='logo'
+            alt='logo'
+            src='https://cdn.dribbble.com/users/4467599/screenshots/11350206/swiggy-logo_4x.jpg'
+        />
+    </a>
+}
 
-const h3 = React.createElement(
-    "h3",
-    {
-        id: "h3",
-        key: "h3",
-    },
-    "Namaste React"
-);
-
-const title = React.createElement(
-    "div",
-    {
-        className: "title",
-    },
-    [h1, h2, h3]
-);
-
-//JSX
-const jsx = (
-    <div className='title'>
-        <h1 id='jh1' customeattribute1="hello1">hello1</h1>
-        <h2 id='jh2' customeattribute2="hello2">hello2</h2>
-        <h3 id='jh3' customeattribute3="hello3">hello3</h3>
-    </div>
-)
-
-//Functional Component //Composing component > when you use component inside another component
-const FunctionalComponent = () => {
+const Header = () => {
     return (
-        <div>{jsx}</div>
+        <div className='header'>
+            <Title />
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
     )
 }
 
-const mergeElements = (
-    <div>
-        {title}
-        {jsx}
-        <FunctionalComponent />
-        {/* <FunctionalComponent></FunctionalComponent> */}
-    </div>
-)
+const Body = () => {
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+}
 
-//passing a react element inside the root
-// root.render(title);
-// root.render(jsx);
-root.render(mergeElements);
-// root.render(<FunctionalComponent />);
+const Footer = () => {
+    return <h4>Footer</h4>
+}
+
+const AppLayout = () => {
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppLayout />)
