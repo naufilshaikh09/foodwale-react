6757aebe35a6ffe95c7efb64e125e8157c7375a7
Naufil Shaikh <snaufil09@gmail.com>
2023-01-15 14:58:59 +0530

    Chapter 6
    Update the UI and Shimmer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,13 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 const Footer = () => {
-    return <h4>Footer</h4>
+    return (
+        // <Card.Footer className="text-muted">@2023 FoodWale</Card.Footer>
+        <Card className="text-center">
+            <Card.Footer className="text-muted">@2023 FoodWale</Card.Footer>
+        </Card>
+    )
 }
 
 export default Footer;
