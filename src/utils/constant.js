b4c8b0a14c0d2e05d563b1ea6e4ebfa73e1cdf2b
Naufil Shaikh <snaufil09@gmail.com>
2023-02-23 09:20:07 +0530

    update swiggy url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,8 @@
 export const IMG_CDN_URL = 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/';
 
-export const RESTAURANT_API = 'https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.0120338&lng=72.51075399999999&page_type=DESKTOP_WEB_LISTING';
+export const RESTAURANT_API = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.0120338&lng=72.51075399999999&page_type=DESKTOP_WEB_LISTING';
 
-export const RESTAURANT_MENU_API = "https://corsanywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/v4/full?lat=23.0120338&lng=72.51075399999999&menuId="
+export const RESTAURANT_MENU_API = "https://www.swiggy.com/dapi/menu/v4/full?lat=23.0120338&lng=72.51075399999999&menuId="
 
 export const RESTAURANT_CAROUSELS_API = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_520,h_520/";
 
